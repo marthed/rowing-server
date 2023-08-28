363ed77e5fda536904db39330caad9873e3c9555
Martin Hedlund <martinhedlund@live.com>
2023-08-28 18:53:34 +0200

    P9

--- hitsRanking.js
+++ hitsRanking.js
@@ -5,6 +5,13 @@ const resultsFolderPath = "./results";
 
 let participantData = [];
 
+// Function to convert seconds to minutes and seconds format
+function toMinutesAndSeconds(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.round(seconds - minutes * 60);
+  return `${minutes}m ${remainingSeconds}s`;
+}
+
 // Read all files in the results folder
 fs.readdir(resultsFolderPath, (err, files) => {
   if (err) {
@@ -24,12 +31,14 @@ fs.readdir(resultsFolderPath, (err, files) => {
 
       const methodIndex = labels.findIndex((label) => label === "method");
       const modeIndex = labels.findIndex((label) => label === "mode");
+      const hitIndex = labels.findIndex((label) => label === "hit");
       const timeIndex = labels.findIndex((label) => label === "time");
 
       if (
         lines.length < 2 ||
         methodIndex === -1 ||
         modeIndex === -1 ||
+        hitIndex === -1 ||
         timeIndex === -1
       ) {
         console.warn(`Warning: Skipping file ${file} due to missing data.`);
@@ -39,6 +48,13 @@ fs.readdir(resultsFolderPath, (err, files) => {
       const secondToLastRow = lines[lines.length - 2].split(",");
       const method = secondToLastRow[methodIndex];
       const mode = secondToLastRow[modeIndex];
+
+      // Count the number of 'True' values in the hit column
+      const hitCount = lines.slice(1).reduce((acc, row) => {
+        const hitValue = row.split(",")[hitIndex];
+        return acc + (hitValue === "True" ? 1 : 0); // Capitalized 'True'
+      }, 0);
+
       const time = parseFloat(secondToLastRow[timeIndex]);
       const participant = file.split("_")[0];
 
@@ -46,43 +62,52 @@ fs.readdir(resultsFolderPath, (err, files) => {
         participant,
         method,
         mode,
+        hitCount,
         time,
       });
     }
   }
 
-  function toMinutesAndSeconds(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.round(seconds - minutes * 60);
-    return `${minutes}m ${remainingSeconds}s`;
-  }
-
-  // Generate rankings for each method and mode
+  // Generate rankings for each method and mode based on hit counts and time
   let rankingMap = {};
   for (const data of participantData) {
-    const key = `${data.method}-${data.mode}`;
+    const key = `${data.method}-${data.mode}`; // Use '-' instead of '_'
     if (!rankingMap[key]) rankingMap[key] = [];
     rankingMap[key].push(data);
   }
 
   for (const key in rankingMap) {
-    const [methodValue, modeValue] = key.split("-");
+    const [methodValue, modeValue] = key.split("-"); // Split with '-'
     console.log(`Ranking for method: ${methodValue} and mode: ${modeValue}`);
-    const sortedRankings = rankingMap[key].sort((a, b) => a.time - b.time);
+
+    const sortedRankings = rankingMap[key].sort(
+      (a, b) => b.hitCount - a.hitCount || a.time - b.time
+    ); // Rank by hitCount, then by time
 
     sortedRankings.forEach((data, idx) => {
       console.log(
-        `${idx + 1}. ${data.participant} ${toMinutesAndSeconds(data.time)}`
+        `${idx + 1}. ${data.participant} with ${
+          data.hitCount
+        } hits and time ${toMinutesAndSeconds(data.time)}`
       );
     });
 
-    // Compute and print mean time
+    // Compute and print mean hit count and mean time
+    const totalHits = sortedRankings.reduce(
+      (acc, entry) => acc + entry.hitCount,
+      0
+    );
     const totalTimes = sortedRankings.reduce(
       (acc, entry) => acc + entry.time,
       0
     );
+    const meanHits = totalHits / sortedRankings.length;
     const meanTime = totalTimes / sortedRankings.length;
-    console.log(`Mean time: ${toMinutesAndSeconds(meanTime)}`);
+    console.log(
+      `Mean hits: ${meanHits.toFixed(2)}, Mean time: ${toMinutesAndSeconds(
+        meanTime
+      )}`
+    );
     console.log("\n");
   }
 });
